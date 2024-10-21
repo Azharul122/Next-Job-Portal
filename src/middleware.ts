10a86bd031107ba66099afba4a1d7f9e06cb53ce
Azharul Islam <azharulip@gmail.com>
2024-10-21 14:22:08 +0600

    middleware issue resolved

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,31 @@
+import { getCsrfToken } from "next-auth/react";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedPaths = ["/dashboard/:path*", "/jobs/:path*", "/companies/:path*", "/settings/:path*",'/user-profile/:path*'];
+const protectedPaths = [
+  "/dashboard/:path*",
+  "/jobs/:path*",
+  "/companies/:path*",
+  "/settings/:path*",
+  "/user-profile/:path*",
+];
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const isProtected = protectedPaths.some(path => {
-
+  const isProtected = protectedPaths.some((path) => {
     const regex = new RegExp(
-      '^' + path.replace(/:\w+\*/g, '.*').replace(/\//g, '\\/') + '$'
+      "^" + path.replace(/:\w+\*/g, ".*").replace(/\//g, "\\/") + "$"
     );
     return regex.test(pathname);
   });
 
   if (isProtected) {
- 
+    const token = await getCsrfToken();
 
-    const user = request.cookies.get(`${process.env.NODE_ENV==="development"?"authjs.session-token":"__Secure-authjs.session-token"}`);
-
-    console.log(user)
-
-    if (!user) {
-
+    if (!token) {
       const signInUrl = new URL("/sign-in", request.url);
-      signInUrl.searchParams.set("redirect", pathname);
+      signInUrl.searchParams.set("redirect", pathname); 
       return NextResponse.redirect(signInUrl);
     }
   }
@@ -33,11 +34,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/dashboard/:path*',
-    '/jobs/:path*',
-    '/companies/:path*',
-    '/settings/:path*',
-    '/user-profile/:path*'
-  ],
+  matcher: protectedPaths,
 };
